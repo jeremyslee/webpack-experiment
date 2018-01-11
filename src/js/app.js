0337f4be7c8affa94199cbda6b3b9fc5a8f2b89a
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-10 21:52:57 -0800

    Install react react-dom and update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+render(
+  <Hello />,
+  document.getElementById('app')
+);
